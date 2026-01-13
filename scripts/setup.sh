e72451dc7d7c1affa6366e0aac44ede5713f19a1
rib3ni <renanaugustolima@gmail.com>
2026-01-13 11:21:31 +0000

    commit 1

---
+++ scripts/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "A atualizar índice de pacotes..."
+sudo apt update
+
+echo "A instalar curl..."
+sudo apt install -y curl
+
+echo "Instalação concluída com sucesso!"
--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Atualizar índice de pacotes
-sudo apt update
-
-# Instalar curl
-sudo apt install -y curl
