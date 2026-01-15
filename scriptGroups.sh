42f52b5841cb0cd05b6d623d66cc47d2527a4bc2
rib3ni <renanaugustolima@gmail.com>
2026-01-15 10:08:53 +0000

    Esqueleto do script

--- scriptGroups.sh
+++ scriptGroups.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-
+ACTION="$1"
+shift
+case "$ACTION" in
+list)
+# lógica de listagem de grupos existentes
+;;
+mine)
+# lógica de listagem dos meus grupos
+;;
+add|del)
+# lógica de alteração de grupos
+;;
+*)
+echo "Uso: $0 {list|mine|add|del} [grupos]"
+exit 1
+;;
+esac
