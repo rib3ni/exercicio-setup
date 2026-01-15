73309fae156bec5079082709271961cf5b599e7c
Rodney Lima Pereira <limapereirarodney@gmail.com>
2026-01-15 10:55:22 +0000

    README.md

--- scriptGroups.sh
+++ scriptGroups.sh
@@ -1,19 +1,56 @@
+
 #!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "Uso: ./scriptGroups.sh {list|mine|add|del} [grupos]"
+    exit 1
+fi
 
 ACTION="$1"
 shift
+
 case "$ACTION" in
-list)
-# lógica de listagem de grupos existentes
-;;
-mine)
-# lógica de listagem dos meus grupos
-;;
-add|del)
-# lógica de alteração de grupos
-;;
-*)
-echo "Uso: $0 {list|mine|add|del} [grupos]"
-exit 1
-;;
+    list)
+        ;;
+    mine)
+        ;;
+    add)
+        if [ $# -lt 1 ]; then
+            echo "Erro: indique pelo menos um grupo"
+            exit 1
+        fi
+
+        CURRENT_USER=$(whoami)
+
+        for GROUP in "$@"; do
+            if getent group "$GROUP" > /dev/null; then
+                echo "A adicionar $CURRENT_USER ao grupo $GROUP"
+                usermod -aG "$GROUP" "$CURRENT_USER"
+            else
+                echo "Erro: grupo '$GROUP' não existe"
+            fi
+        done
+        ;;
+    del)
+        if [ $# -lt 1 ]; then
+            echo "Erro: indique pelo menos um grupo"
+            exit 1
+        fi
+
+        CURRENT_USER=$(whoami)
+
+        for GROUP in "$@"; do
+            if getent group "$GROUP" > /dev/null; then
+                echo "A remover $CURRENT_USER do grupo $GROUP"
+                gpasswd -d "$CURRENT_USER" "$GROUP"
+            else
+                echo "Erro: grupo '$GROUP' não existe"
+            fi
+        done
+        ;;
+    *)
+        echo "Uso: ./scriptGroups.sh {list|mine|add|del} [grupos]"
+        exit 1
+        ;;
+
 esac
